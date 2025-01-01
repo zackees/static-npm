ef2068abf1fad7aa1b2734370ea2cf8c49f54402
zackees <z@zackees.com>
2024-12-31 17:46:18 -0800

    feat: add httpx dependency and update live server tests

--- tests/test_live_server.py
+++ tests/test_live_server.py
@@ -2,8 +2,11 @@
 Unit test file.
 """
 
+import time
 import unittest
 from pathlib import Path
+
+import httpx  # type: ignore
 
 from static_npm.npm import Npm
 from static_npm.npx import Npx
@@ -12,6 +15,9 @@ from static_npm.paths import CACHE_DIR
 
 def _get_tool_dir(tool: str) -> Path:
     return CACHE_DIR / tool
+
+
+PORT = 8375
 
 
 class MainTester(unittest.TestCase):
@@ -23,11 +29,23 @@ class MainTester(unittest.TestCase):
         npx = Npx()
         tool_dir = _get_tool_dir("live-server")
         npm.run(["install", "live-server", "--prefix", str(tool_dir)])
-        proc = npx.run(["live-server", "--version", "--prefix", str(tool_dir)])
-        rtn = proc.wait()
-        stdout = proc.stdout
-        self.assertEqual(0, rtn)
-        self.assertIn("live-server", stdout)
+        proc = npx.run(
+            ["--prefix", str(tool_dir), "live-server", f"--port={PORT}", "--no-browser"]
+        )
+
+        timeout = time.time() + 60
+        while time.time() < timeout:
+            try:
+                response = httpx.get(f"http://localhost:{PORT}")
+                if response.status_code == 200:
+                    break
+            except httpx.ConnectError:
+                pass
+            time.sleep(1)
+        else:
+            self.fail("Server did not start.")
+
+        proc.terminate()
 
 
 if __name__ == "__main__":
