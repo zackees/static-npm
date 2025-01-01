83bf0a2d237f1be7ba6df100d09369665ef114a6
zackees <z@zackees.com>
2024-12-31 16:35:58 -0800

    chore: update version to 1.0.2 and modify MANIFEST.in

--- src/static_npm/__init__.py
+++ src/static_npm/__init__.py
@@ -1,5 +1,7 @@
 from .node import Node
 from .npm import Npm
 from .npx import Npx
+from .paths import CACHE_DIR
+from .running_process import RunningProcess
 
-__all__ = ["Npm", "Node", "Npx"]
+__all__ = ["Npm", "Node", "Npx", "RunningProcess", "CACHE_DIR"]
